d06592dfea082c844dc6741a6d025cc87767ed8b
Trinigch <trinigch@gmail.com>
2024-10-28 12:29:19 -0500

    adding projects

--- src/pages/Portfolio.tsx
+++ src/pages/Portfolio.tsx
@@ -2,27 +2,51 @@
 import styled from 'styled-components';
 import Project from '../components/Project'; // Asegúrate de la ruta correcta
 import Job1 from './../assets/img/Gallery.png'
+import Job2 from './../assets/img/bg.jpg'
+import Job3 from './../assets/img/Employee-Tracker.jpeg'
+import Job4 from './../assets/img/Vehicle-Builder.jpg'
+import Job5 from './../assets/img/Themed-Timer.png'
+import Job6 from './../assets/img/register.png'
 
 // Aquí defines los datos de los proyectos
 const projectsData = [
   {
+    title: 'Themed-Timer',
+    deployedUrl: 'https://trinigch.github.io/Themed-Timer/',
+    githubUrl: 'https://github.com/Trinigch/Themed-Timer.git', 
+    imageUrl: Job5,
+  },
+  {
     title: 'Gallery Project',
     deployedUrl: 'https://trinigch.github.io/Galery/',
     githubUrl: 'https://github.com/Trinigch/Galery',
-    imageUrl:Job1,
+    imageUrl: Job1,
   
   },
   {
-    title: 'Shop Project',
-    deployedUrl: 'https://trinigch.github.io/Shop/',
-    githubUrl: 'https://github.com/Trinigch/Shop',
-    imageUrl: './../assets/img/shop.png', // Reemplaza con la ruta correcta
+    title: 'Weather-Dashboard',
+    deployedUrl: '',
+    githubUrl: 'https://github.com/Trinigch/Weather-Dashboard.git',
+    imageUrl: Job2, 
   },
+  {
+    title: 'Employee-Tracker',
+    deployedUrl: 'https://curso-react-app-lista-ga-a835b.web.app',
+    githubUrl: 'https://github.com/Trinigch/Employee-Tracker.git', 
+    imageUrl: Job3, 
+  },
+  {
+    title: 'Vehicle-Builder',
+    deployedUrl: 'https://curso-react-app-lista-ga-a835b.web.app',
+    githubUrl: 'https://github.com/Trinigch/Vehicle-Builder.git', 
+    imageUrl: Job4, 
+  },
+
   {
     title: 'Register App',
     deployedUrl: 'https://curso-react-app-lista-ga-a835b.web.app',
-    githubUrl: 'https://github.com/Trinigch/RegisterApp', // Asegúrate de tener esta URL
-    imageUrl: './../assets/img/register.png', // Reemplaza con la ruta correcta
+    githubUrl: 'https://github.com/Trinigch/RegisterApp', 
+    imageUrl: Job6,
   },
   // Agrega más proyectos aquí
 ];
