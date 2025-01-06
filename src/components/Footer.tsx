9802fa679caf8b7f7bf8a0a9c224e0ab3e4c4883
Trinigch <trinigch@gmail.com>
2025-01-05 22:00:12 -0600

    updatng year in footer

--- src/components/Footer.tsx
+++ src/components/Footer.tsx
@@ -1,7 +1,7 @@
 function Footer() {
   return (
     <footer className="bg-primary text-primary-content p-4">
-      <p>© 2024 Peterson</p>
+      <p>© 2025 Peterson</p>
       <div>
         <a href="https://github.com/username" className="mr-4">GitHub</a>
         <a href="https://linkedin.com/in/username">LinkedIn</a>
