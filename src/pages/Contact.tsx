027082ccb35489f607a5fb99d774b8d3b3402a19
Trinigch <trinigch@gmail.com>
2025-01-05 19:05:57 -0600

    adding emailjs library

--- src/pages/Contact.tsx
+++ src/pages/Contact.tsx
@@ -1,13 +1,17 @@
 import { useState } from 'react';
 import styled from 'styled-components';
+import emailjs from 'emailjs-com';
+
+// Styled components
 const Container = styled.div`
-  width: 100%; /* Asegura que el contenedor ocupe todo el ancho */
-  margin: auto; /* Centra el contenedor */
+  width: 100%;
+  margin: auto;
   border-radius: 10px;
   box-shadow: 0 4px 8px rgba(0, 0, 0, 0.2);
   display: flex;
   overflow: hidden;
 `;
+
 const FormContainer = styled.div`
   margin: 20px;
   padding: 20px;
@@ -37,20 +41,20 @@ const StyledInput = styled.input`
   }
 `;
 
-const StyledTextarea = styled.textarea`
-  width: 100%;
-  padding: 12px;
-  border: 1px solid #ccc;
-  border-radius: 5px;
-  font-size: 16px;
-  resize: none;
+// const StyledTextarea = styled.textarea`
+//   width: 100%;
+//   padding: 12px;
+//   border: 1px solid #ccc;
+//   border-radius: 5px;
+//   font-size: 16px;
+//   resize: none;
 
-  &:focus {
-    border-color: teal;
-    outline: none;
-    box-shadow: 0 0 5px rgba(0, 128, 128, 0.5);
-  }
-`;
+//   &:focus {
+//     border-color: teal;
+//     outline: none;
+//     box-shadow: 0 0 5px rgba(0, 128, 128, 0.5);
+//   }
+// `;
 
 const StyledButton = styled.button`
   margin-top: 15px;
@@ -73,154 +77,111 @@ const ErrorMessage = styled.p`
   margin: 5px 0 0;
 `;
 
-interface Errors {
-  firstName: string;
-  lastName: string;
-  email: string;
-  message: string;
-}
+// Validation messages
+const ERROR_MESSAGES = {
+  required: 'This field is required',
+  email: 'Please enter a valid email address',
+};
 
+// Component
 export default function Contact() {
-  const [firstName, setFirstName] = useState('');
-  const [lastName, setLastName] = useState('');
-  const [email, setEmail] = useState('');
-  const [message, setMessage] = useState('');
-  const [errors, setErrors] = useState<Errors>({
+  const [formData, setFormData] = useState({
     firstName: '',
     lastName: '',
     email: '',
     message: '',
   });
 
-  const handleInputChange = (e: React.ChangeEvent<HTMLInputElement | HTMLTextAreaElement>) => {
-    const { name, value } = e.target;
+  const [errors, setErrors] = useState({
+    firstName: '',
+    lastName: '',
+    email: '',
+    message: '',
+  });
 
-    if (name === 'firstName') {
-      setFirstName(value);
-      setErrors((prev) => ({ ...prev, firstName: '' }));
-    } else if (name === 'lastName') {
-      setLastName(value);
-      setErrors((prev) => ({ ...prev, lastName: '' }));
-    } else if (name === 'email') {
-      setEmail(value);
-      setErrors((prev) => ({ ...prev, email: '' }));
-    } else if (name === 'message') {
-      setMessage(value);
-      setErrors((prev) => ({ ...prev, message: '' }));
-    }
-  };
-
-  const handleBlur = (e: React.ChangeEvent<HTMLInputElement | HTMLTextAreaElement>) => {
-    const { name, value } = e.target;
-
+  const validateField = (name: string, value: string) => {
     if (!value) {
-      setErrors((prev) => ({ ...prev, [name]: 'Este campo es obligatorio' }));
-    } else if (name === 'email') {
+      return ERROR_MESSAGES.required;
+    }
+    if (name === 'email') {
       const emailRegex = /^[^\s@]+@[^\s@]+\.[^\s@]+$/;
       if (!emailRegex.test(value)) {
-        setErrors((prev) => ({ ...prev, email: 'Ingrese un correo electrónico válido' }));
+        return ERROR_MESSAGES.email;
       }
     }
+    return '';
   };
 
-  const handleFormSubmit = (e: React.FormEvent<HTMLFormElement>) => {
-    e.preventDefault();
-    let formIsValid = true;
-    const newErrors: Errors = {
-      firstName: '',
-      lastName: '',
-      email: '',
-      message: '',
-    };
+  const handleInputChange = (e: React.ChangeEvent<HTMLInputElement | HTMLTextAreaElement>) => {
+    const { name, value } = e.target;
+    setFormData((prev) => ({ ...prev, [name]: value }));
+    setErrors((prev) => ({ ...prev, [name]: '' }));
+  };
 
-    if (!firstName) {
-      newErrors.firstName = 'Este campo es obligatorio';
-      formIsValid = false;
-    }
-    if (!lastName) {
-      newErrors.lastName = 'Este campo es obligatorio';
-      formIsValid = false;
-    }
-    if (!email) {
-      newErrors.email = 'Este campo es obligatorio';
-      formIsValid = false;
-    } else {
-      const emailRegex = /^[^\s@]+@[^\s@]+\.[^\s@]+$/;
-      if (!emailRegex.test(email)) {
-        newErrors.email = 'Ingrese un correo electrónico válido';
-        formIsValid = false;
+  const handleBlur = (e: React.FocusEvent<HTMLInputElement | HTMLTextAreaElement>) => {
+    const { name, value } = e.target;
+    const error = validateField(name, value);
+    setErrors((prev) => ({ ...prev, [name]: error }));
+  };
+
+  const handleFormSubmit = async (e: React.FormEvent<HTMLFormElement>) => {
+    e.preventDefault();
+
+    const newErrors = Object.keys(formData).reduce((acc, field) => {
+      // Aseguramos que field es una clave válida de formData
+      if (field in acc) {
+        acc[field as keyof typeof formData] = validateField(field, formData[field as keyof typeof formData]);
       }
-    }
-    if (!message) {
-      newErrors.message = 'Este campo es obligatorio';
-      formIsValid = false;
-    }
+      return acc;
+    }, {} as { [key in keyof typeof formData]: string });
 
     setErrors(newErrors);
 
+    const formIsValid = !Object.values(newErrors).some((error) => error);
+
     if (formIsValid) {
-      alert(`Hola ${firstName} ${lastName}, tu mensaje ha sido enviado.`);
-      setFirstName('');
-      setLastName('');
-      setEmail('');
-      setMessage('');
+      try {
+        const { firstName, lastName, email, message } = formData;
+
+        // Replace with your EmailJS credentials
+        const SERVICE_ID = 'service_9nyzw93';
+        const TEMPLATE_ID = 'template_3alafqd';
+        const USER_ID = 'jFodaExO7SRQKsoqG';
+
+        await emailjs.send(SERVICE_ID, TEMPLATE_ID, { firstName, lastName, email, message }, USER_ID);
+
+        alert(`Thank you, ${firstName} ${lastName}. Your message has been sent successfully.`);
+        setFormData({ firstName: '', lastName: '', email: '', message: '' });
+      } catch (error) {
+        console.error('Error sending email:', error);
+        alert('An error occurred while sending your message. Please try again later.');
+      }
     }
   };
 
   return (
     <Container>
-    <div className="min-h-screen w-screen flex items-center justify-center">
-      <FormContainer>
-        <h1 className="text-4xl font-bold text-center">Contact Us</h1>
-        <form onSubmit={handleFormSubmit}>
-          <InputContainer>
-            <StyledInput
-              value={firstName}
-              name="firstName"
-              onChange={handleInputChange}
-              onBlur={handleBlur}
-              type="text"
-              placeholder="First Name"
-            />
-            {errors.firstName && <ErrorMessage>{errors.firstName}</ErrorMessage>}
-          </InputContainer>
-          <InputContainer>
-            <StyledInput
-              value={lastName}
-              name="lastName"
-              onChange={handleInputChange}
-              onBlur={handleBlur}
-              type="text"
-              placeholder="Last Name"
-            />
-            {errors.lastName && <ErrorMessage>{errors.lastName}</ErrorMessage>}
-          </InputContainer>
-          <InputContainer>
-            <StyledInput
-              value={email}
-              name="email"
-              onChange={handleInputChange}
-              onBlur={handleBlur}
-              type="email"
-              placeholder="Email Address"
-            />
-            {errors.email && <ErrorMessage>{errors.email}</ErrorMessage>}
-          </InputContainer>
-          <InputContainer>
-            <StyledTextarea
-              value={message}
-              name="message"
-              onChange={handleInputChange}
-              onBlur={handleBlur}
-              placeholder="Your Message"
-              rows={4}
-            />
-            {errors.message && <ErrorMessage>{errors.message}</ErrorMessage>}
-          </InputContainer>
-          <StyledButton type="submit">Submit</StyledButton>
-        </form>
-      </FormContainer>
-    </div>
+      <div className="min-h-screen w-screen flex items-center justify-center">
+        <FormContainer>
+          <h1 className="text-4xl font-bold text-center">Contact Us</h1>
+          <form onSubmit={handleFormSubmit}>
+            {['firstName', 'lastName', 'email', 'message'].map((field) => (
+              <InputContainer key={field}>
+                <StyledInput
+                  value={formData[field as keyof typeof formData]}
+                  name={field}
+                  onChange={handleInputChange}
+                  onBlur={handleBlur}
+                  type={field === 'email' ? 'email' : 'text'}
+                  placeholder={field.charAt(0).toUpperCase() + field.slice(1)}
+                />
+                {errors[field as keyof typeof errors] && <ErrorMessage>{errors[field as keyof typeof errors]}</ErrorMessage>}
+              </InputContainer>
+            ))}
+            <StyledButton type="submit">Send</StyledButton>
+          </form>
+        </FormContainer>
+      </div>
     </Container>
   );
 }
